6403a4b32f9c3cd896985b2fcdf0c05cbfc71cf2
funk443 <funk443@yandex.com>
2022-04-25 07:59:03 +0800

    Added ping funtion.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default ()
 intents.members = True
 
-bot = commands.Bot (command_prefix = "269", intents = intents)
+bot = commands.Bot (command_prefix = "a269 ", intents = intents)
 
 @bot.event
 async def on_ready ():
@@ -21,6 +21,10 @@ async def on_member_remove (member):
   print (f"{member} left!")
   channel = bot.get_channel (967880010281123913)
   await channel.send (f"{member} left!")
+
+@bot.command ()
+async def ping (ctx):
+  await ctx.send (f"{round (bot.latency * 1000)} ms")
 
 bot.run ("OTY3OTE0OTk1NTA3NjU4Nzcy.YmXPFQ.q-yNgGqf_D9Mc_wTbWcAOBOfgtA")
 
