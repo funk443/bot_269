ef14adecc88aed45f94c02fbc39e24fb82e4d8f7
funk443 <funk443@yandex.com>
2022-04-25 08:12:32 +0800

    Added config file, and replace some things in bot.py.

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import json
+
+with open ("config.json", mode = "r", encoding = "utf8") as conf:
+  conf_data = json.load (conf)
 
 intents = discord.Intents.default ()
 intents.members = True
@@ -13,18 +17,18 @@ async def on_ready ():
 @bot.event
 async def on_member_join (member):
   print (f"{member} joined!")
-  channel = bot.get_channel (967880010281123913)
+  channel = bot.get_channel (int(conf_data["general"]))
   await channel.send (f"{member} joined!")
   
 @bot.event
 async def on_member_remove (member):
   print (f"{member} left!")
-  channel = bot.get_channel (967880010281123913)
+  channel = bot.get_channel (int(conf_data["general"]))
   await channel.send (f"{member} left!")
 
 @bot.command ()
 async def ping (ctx):
   await ctx.send (f"{round (bot.latency * 1000)} ms")
 
-bot.run ("OTY3OTE0OTk1NTA3NjU4Nzcy.YmXPFQ.q-yNgGqf_D9Mc_wTbWcAOBOfgtA")
+bot.run (conf_data["TOKEN"])
 
