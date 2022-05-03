23624f0ceb2fde640ab48292cb81dca0f7d70135
funk443 <funk443@yandex.com>
2022-05-04 01:13:31 +0800

    minor changes

---
+++ cmds/events.py
@@ -0,0 +1,45 @@
+import discord
+import json
+import random
+from discord.ext import commands
+from core.classes import cog_ext
+
+js = open ("config.json")
+conf = json.load (js)
+js.close ()
+
+class events (cog_ext):
+  @commands.Cog.listener ()
+  async def on_member_join (self, member):
+    channel = self.bot.get_channel (conf["general"])
+    await channel.send (f"Welcome, {member.mention}")
+
+  @commands.Cog.listener ()
+  async def on_member_leave (self, member):
+    channel = self.bot.get_channel (conf["general"])
+    await channel.send (f"Goodbye, {member.mention}")
+  
+  @commands.Cog.listener ()
+  async def on_message (self, message):
+    if (message.author == self.bot.user):
+      return
+
+    if ("linux" in message.content.lower ().split ()):
+      f = open (conf["GNULinux"], "r")
+      await message.channel.send (f.read ())
+      f.close ()
+  
+  @commands.Cog.listener ()
+  async def on_message_edit (self, before, after):
+    if (before.author == self.bot.user):
+      return
+  
+    word = random.choice (conf["kai"])
+    await before.channel.send (f"{before.author.mention}{word}")
+
+  @commands.Cog.listener ()
+  async def on_command_error (self, ctx, error):
+    await ctx.send (random.choice (conf["wtf"]))
+
+def setup (bot):
+  bot.add_cog (events (bot))
