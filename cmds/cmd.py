c0dec891b7b04fc0fc5984d2ee6147d7cee89ea6
funk443 <funk443@yandex.com>
2022-05-04 20:34:19 +0800

    can now modify food list using commands.

--- cmds/cmd.py
+++ cmds/cmd.py
@@ -12,12 +12,12 @@ js.close ()
 class cmd (cog_ext):
   @commands.command ()
   async def reply (self, ctx, key, content_1 = None, content_2 = None):
-    if (f"data_{ctx.guild.id}.json" not in os.listdir ("./datas")):
-      f = open (f"./datas/data_{ctx.guild.id}.json", "w")
+    if (f"data_reply_{ctx.guild.id}.json" not in os.listdir ("./datas")):
+      f = open (f"./datas/data_reply_{ctx.guild.id}.json", "w")
       json.dump ({}, f)
       f.close ()
 
-    f = open (f"./datas/data_{ctx.guild.id}.json", "r")
+    f = open (f"./datas/data_reply_{ctx.guild.id}.json", "r")
     data = json.load (f)
     f.close ()
 
@@ -25,7 +25,7 @@ class cmd (cog_ext):
       if (content_2 == None):
         await ctx.send ("你是要我加什麼啦")
       else:
-        f = open (f"./datas/data_{ctx.guild.id}.json", "w")
+        f = open (f"./datas/data_reply_{ctx.guild.id}.json", "w")
         data[str (content_1)] = str (content_2)
         json.dump (data, f)
         f.close ()
@@ -37,7 +37,7 @@ class cmd (cog_ext):
         await ctx.send ("你沒給我東西我是要刪什麼啦")
       else:
         if (content_1 in data):
-          f = open (f"./datas/data_{ctx.guild.id}.json", "w")
+          f = open (f"./datas/data_reply_{ctx.guild.id}.json", "w")
           data.pop (content_1)
           json.dump (data, f)
           f.close ()
@@ -66,10 +66,46 @@ class cmd (cog_ext):
 
   @commands.command ()
   async def 吃什麼 (self, ctx, *option):
-    if (option == None):
-      await ctx.send ("你沒給我選項我是要怎麼選")
-    else:
-      await ctx.send (f"吃 {random.choice (option)} 好了")
+    option = list (option)
+    if (f"data_food_{ctx.guild.id}.json" not in os.listdir ("./datas")):
+      f = open (f"./datas/data_food_{ctx.guild.id}.json", "w")
+      json.dump ({"food":[]}, f)
+      f.close ()
+
+    f = open (f"./datas/data_food_{ctx.guild.id}.json", "r")
+    food = json.load (f)
+    f.close ()
+
+    if ("add" in option):
+      f = open (f"./datas/data_food_{ctx.guild.id}.json", "w")
+
+      for i in option[1:]:
+        food["food"].append (i)
+
+      json.dump (food, f)
+      f.close ()
+    elif ("del" in option):
+      f = open (f"./datas/data_food_{ctx.guild.id}.json", "w")
+      for i in option[1:]:
+        if (i not in food["food"]):
+          await ctx.send (f"先生，沒有{i}")
+        else:
+          food["food"].remove (i)
+      json.dump (food, f)
+      f.close ()
+    elif ("clr" in option):
+      f = open (f"./datas/data_food_{ctx.guild.id}.json", "w")
+      json.dump ({"food":[]}, f)
+      f.close ()
+    else:      
+      if (option == []):
+        food_option = food["food"]
+        if (len (food_option) == 0):
+          await ctx.send ("預設清單是空的我要怎麼選啦，用```a269 吃什麼 add```來加入食物")
+        else:
+          await ctx.send (f"沒有給選項，我會從 [{', '.join (food_option)}] 裡選。吃 {random.choice (food_option)} 好了")
+      else:
+        await ctx.send (f"吃 {random.choice (option)} 好了")
       
     
 def setup (bot):
--- cmds/help.py
+++ cmds/help.py
@@ -3,14 +3,17 @@ import json
 import os
 from core.classes import cog_ext
 from discord.ext import commands
-
+from orgparse import load, loads
 
 class help_commands (cog_ext):
   @commands.command ()
   async def help (self, ctx, cate = None):
     await ctx.send ("還在寫，不要急。先給你可以用的指令列表:")
-    for i in (self.bot.commands):
-      await ctx.send (f"```{i}```")
+    help_file = load ("./resources/help.org")
+
+    if (cate == None):
+      for i in help_file:
+        await ctx.send (i)
     
 def setup (bot):
   bot.add_cog (help_commands (bot))
